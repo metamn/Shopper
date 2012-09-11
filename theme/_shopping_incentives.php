ac7663704be4dd52c238485caf04cb8f5abfa4a0
Your Name Here <bartus.csongor@gmail.com>
2012-09-11 10:59:44 +0300

    Shopping incentives done

--- theme/_shopping_incentives.php
+++ theme/_shopping_incentives.php
@@ -5,32 +5,12 @@
 		<?php include '_add_to_cart.php' ?>
 	</div>
 	
-	<dl>
-		<dt>Politica de preturi Smuff</dt>
-		<dd>
-			<ol>
-				<li>
-					Noi suntem primii in Romania care va aduc cele mai noi gadgeturi si gizmouri premium din intreaga lume.
-					<p>
-					La aceste produse noi practicam preturi ca si cum vi le-ati cumpara direct din strainatate. In plus, la acelasi pret la noi primiti servicii impecabile — suport si garantie pe plan local.
-					</p>
-				</li>
-				<li>
-					Cu timpul unele produse de pe Smuff devin foarte populare — tigara electronica, Parror IR Drone, tricourile elctronice etc — si sunt vandute de catre alte magazine din tara la preturi discount.
-				</li>
-				<li>
-					La acest capitol incercam sa tinem pasul prin reduceri. Insa misiunea noastra ramane de a va aduce noutatiile la preturi accesibile
-				</li>
-			</ol>
-		</dd>
-		<dt>Costuri de livrare</dt>
-		<dd>
-			<p>
-				Noi am ales preturi cat se poate de mici in magazinul Smuff. Din acest motiv costurile de livrare nu pot fi incorporate in pretul produselor.
-			</p>
-		</dd>
-		<dt>Garantie si retur</dt>
-		<dd>....
-		</dd>
-	</dl>
+	<div id="incentives">
+		<ul>
+			<li>Preturi corecte, servicii stabile si asistenta rapida.</li>
+			<li>Shopping simplu fara procedura de inregistrare.</li>
+			<li>Satisfactie sau returnare produs in 10 zile.</li>
+			<li>Garantie tehnica cel putin 1 an.</li>
+		</ul>
+	</div>
 </aside>
