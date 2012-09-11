513bfc8c931db28260207e9edf0b7da85dd0ffd5
Your Name Here <bartus.csongor@gmail.com>
2012-09-11 12:05:23 +0300

    Wishlist, subscribe to newsletter added

--- theme/_add_to_wishlist.php
+++ theme/_add_to_wishlist.php
@@ -1,1 +1,9 @@
-wishlist ....
+<aside id="add-to-wishlist">
+	<div id="incentives">
+		<dl>
+			<dt>Adaug la lista mea de dorinte</dt>
+			<dd>Smuff ma contacteaza cand aceste produse sunt reduse.</dd>
+		</dl>
+	</div>
+	<div id="action">&#10084;</div>
+</aside>
--- theme/_subscribe_to_newsletter.php
+++ theme/_subscribe_to_newsletter.php
@@ -1,1 +1,9 @@
-newsletter ...
+<aside id="subscribe-to-newsletter">
+	<div id="incentives">
+		<dl>
+			<dt>Ma abonez la stirile despre acest produs</dt>
+			<dd>Smuff va trimite un email cand se schimba ceva la acest produs..</dd>
+		</dl>
+	</div>
+	<div id="action">&#9993;</div>
+</aside>
