f59b0f3b466177a425db821d2ec8edf66ff67cc4
Your Name Here <bartus.csongor@gmail.com>
2012-09-11 18:33:33 +0300

    Shopping incentives done

--- theme/_shopping_incentives.php
+++ theme/_shopping_incentives.php
@@ -1,16 +1,14 @@
 <aside id="shopping-incentives">
 	<div id="action">
 		<h2>De ce merita sa cumpar de la Smuff?</h2>
-		
-		<?php include '_add_to_cart.php' ?>
 	</div>
 	
 	<div id="incentives">
 		<ul>
-			<li>Preturi corecte, servicii stabile si asistenta rapida.</li>
-			<li>Shopping simplu fara procedura de inregistrare.</li>
-			<li>Satisfactie sau returnare produs in 10 zile.</li>
-			<li>Garantie tehnica cel putin 1 an.</li>
+			<li><span>Preturi corecte,</span> servicii stabile si asistenta rapida.</li>
+			<li><span>Shopping simplu</span> fara procedura de inregistrare.</li>
+			<li><span>Satisfactie</span> sau returnare produs in 10 zile.</li>
+			<li><span>Garantie tehnica</span> cel putin 1 an.</li>
 		</ul>
 	</div>
 </aside>
